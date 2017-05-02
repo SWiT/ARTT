65b93239fdc93e04d5ac2a7ca443959faca150c4
SWiT <switlikm@gmail.com>
2017-05-02 12:54:33 -0400

    AR figure

--- figure.py
+++ figure.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     print "AR Figure"
 
     aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
-    markersize = 24
+    markersize = 22
     blackborder = 1
-    whiteborder = 100/8
+    whiteborder = 1
 
-    for markerid in range(0, 6):
+    for markerid in range(0, 100):
         marker = cv2.cvtColor(aruco.drawMarker(aruco_dict, markerid, markersize,borderBits=blackborder), cv2.COLOR_GRAY2BGR) #borderBits
-        marker = cv2.copyMakeBorder(marker, whiteborder, whiteborder, whiteborder, whiteborder, cv2.BORDER_CONSTANT, value=[255,255,255])
-        imagefile = "images/aruco_4X4_" + str(markerid)+".png"
+        if whiteborder > 0:
+            marker = cv2.copyMakeBorder(marker, whiteborder, whiteborder, whiteborder, whiteborder, cv2.BORDER_CONSTANT, value=[255,255,255])
+        imagefile = "images/4X4_" + str(markerid)+".png"
         cv2.imwrite(imagefile, marker)
         print "wrote: " + imagefile

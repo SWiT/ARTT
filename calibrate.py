b27be77a117ced7c280ecf6aa3209b2c9d9ffbf5
SWiT <switlikm@gmail.com>
2017-05-29 18:21:46 -0400

    calibrate script

--- calibrate.py
+++ calibrate.py
@@ -5,8 +5,17 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 
-class Calibrate:
+class Calibration:
     def __init__(self):
+        self.COLOR_WHITE = (255,255,255)
+        self.COLOR_BLUE = (255,0,0)
+        self.COLOR_LBLUE = (255, 200, 100)
+        self.COLOR_GREEN = (0,240,0)
+        self.COLOR_RED = (0,0,255)
+        self.COLOR_YELLOW = (29,227,245)
+        self.COLOR_PURPLE = (224,27,217)
+        self.COLOR_GRAY = (127,127,127)
+
         cols = 6
         rows = 4
         squaresize = 100
@@ -15,7 +24,7 @@ class Calibrate:
         self.boardwidth = 800
         self.boardheight = 600
 
-        self.displaySize = 70
+        self.displaySize = 60
 
         self.corners            = []
         self.ids                = []
@@ -32,15 +41,21 @@ class Calibrate:
         self.calibrationimg = self.board.draw((self.boardwidth,self.boardheight), marginSize=margin)
         cv2.imwrite('calibration_charuco.png', self.calibrationimg)
 
+        #Start capturing images for calibration
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
         self.image     = None
         self.grayimage = None
 
+        self.regionsX = 5
+        self.regionsY = 5
+
+
         return
 
-    def scan(self, image):
-        self.image = image
-        #convert to grayscale
-        self.grayimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    def scan(self):
         # Detect markers in the image
         self.corners, self.ids, self.rejectedImgPoints = aruco.detectMarkers(self.grayimage, self.aruco_dict, parameters=self.parameters)
         # If come markers were found.
@@ -57,48 +72,54 @@ class Calibrate:
 
         return
 
-    def draw(self, image):
-        aruco.drawDetectedMarkers(image, self.corners, self.ids)
-        return image
+    def draw(self):
+        # Draw detected markers
+        aruco.drawDetectedMarkers(self.image, self.corners, self.ids)
 
-    def resize(self, image):
+        #Draw region of interest
+        h,w,d = self.image.shape
+        pt0 = (0,0)
+        pt1 = (w-1,h-1)
+        cv2.rectangle(self.image, pt0, pt1, self.COLOR_PURPLE, 1)
+        return
+
+    def resize(self):
         # Resize output image
-        if np.size(image,1) > 0 and np.size(image,0) > 0 and 0 < self.displaySize < 100:
+        if np.size(self.image,1) > 0 and np.size(self.image,0) > 0 and 0 < self.displaySize < 100:
             r = float(self.displaySize)/100
-            image = cv2.resize(image, (0,0), fx=r, fy=r)
-        return image
+            self.image = cv2.resize(self.image, (0,0), fx=r, fy=r)
+        return
+
+    def getFrame(self):
+        # Get the next frame.
+        ret,self.image = self.cap.read()
+        # Convert to grayscale.
+        self.grayimage = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        return
 
 if __name__ == "__main__":
     print "SWiT's Calibration Script (Q to Exit)"
 
     cv2.namedWindow("Calibration")
-    #cv2.startWindowThread()
+    #cv2.startWindowThread() #This and waitKey don't like each other. Not sure why.
 
-    calibrate = Calibrate()
-
-    #Start capturing images for calibration
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cal = Calibration()
 
     while True:
         # Get the next frame.
-        ret,image = cap.read()
+        cal.getFrame()
 
+        cal.scan()
 
-        calibrate.scan(image)
-
-        image = calibrate.draw(image)
+        cal.draw()
 
         # Display the image or frame of video
-        image = calibrate.resize(image)
-        cv2.imshow('Calibration',image)
+        cal.resize()
+        cv2.imshow('Calibration', cal.image)
 
         #Exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-
 
 #    if False:
 #        print("Calibrating...")
@@ -110,6 +131,6 @@ if __name__ == "__main__":
 #        except:
 #            cap.release()
 
-    cap.release()
+    cal.cap.release()
     cv2.destroyAllWindows()
     print("Exit...")
